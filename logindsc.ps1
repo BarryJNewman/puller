c1ca319116bda9aeef161ecd0c3e653d8bb5faac
Barry J Newman <barry.newman@microsoft.com>
2025-07-09 17:55:28 -0400

    Update logindsc.ps1

--- logindsc.ps1
+++ logindsc.ps1
@@ -1,13 +1,5 @@
 $ThemePath = "C:\Windows\Resources\Themes\avd-dark.theme"
 $CurrentTheme = (Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes").CurrentTheme
-
-
-    Start-Process -FilePath $ThemePath
-    Start-Sleep -Seconds 3
-    Stop-Process -Name "SystemSettings" -Force
-    Write-Output "Theme has been changed to avd-dark."
-
-
 $groupName = "docker-users"
 $currentUserName = [System.Environment]::UserName
 $group = Get-LocalGroup -Name $groupName -ErrorAction SilentlyContinue
@@ -41,35 +33,6 @@ if ($group) {
                 New-ItemProperty -Path $registryPath -Name $propertyName -Value 1 -PropertyType DWORD
                 Write-Output "Restart apps feature is now enabled and configured."
             }
-
-            $uri="https://github.com/coder/coder/releases/download/v2.9.1/coder_2.9.1_windows_amd64_installer.exe"
-                Invoke-WebRequest -Uri $uri -OutFile "C:\Windows\Temp\coder_2.9.1_windows_amd64_installer.exe"
-                $uri="https://github.com/coder/coder/releases/download/v2.9.1/coder_2.9.1_checksums.txt"
-                Invoke-WebRequest -Uri $uri -OutFile "C:\Windows\Temp\coder_2.9.1_checksums.txt"
-
-                $checksum = (Get-Content "C:\Windows\Temp\coder_2.9.1_checksums.txt" | Select-String -Pattern "coder_2.9.1_windows_amd64_installer.exe").ToString().Split()[0]
-                $checksum_validation = Get-FileHash -Path "C:\Windows\Temp\coder_2.9.1_windows_amd64_installer.exe" -Algorithm SHA256
-
-                if ($checksum -eq $checksum_validation.Hash) {
-                    # Install coder
-                    Start-Process -FilePath "C:\Windows\Temp\coder_2.9.1_windows_amd64_installer.exe" -ArgumentList "/S" -Wait
-                } else {
-                    Write-Host "Checksum validation failed"
-                    exit 1
-                }
-
-                New-NetFirewallRule -DisplayName "Allow Coder Executable" -Direction Inbound -Program "C:\program files\coder\bin\coder.exe" -Action Allow
-                
-                Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-                Start-Process -FilePath "C:\Program Files\Coder\bin\coder" -ArgumentList "server" -WindowStyle Hidden
-                
-                Set-Content -Path "C:\Program Files\Coder\CoderInit.ps1" -Value $script
-
-                # create registry key to run coder on startup for all users
-                $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-                $regName = "Coder"
-                $regValue = 'powershell -ExecutionPolicy Bypass -File "C:\Program Files\Coder\CoderInit.ps1"'
-                New-ItemProperty -Path $regPath -Name $regName -Value $regValue -PropertyType String -Force
 
             msg $currentUserName "System will be rebooting in 10 seconds to finish Dev setup for $currentUserName. Please log back in to continue."
             Start-Sleep 10
